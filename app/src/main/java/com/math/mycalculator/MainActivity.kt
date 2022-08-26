d388e6c48ecea0b6d4e2d2437306aad713427205
math-ferreira <mat.s.ferreira@gmail.com>
2022-08-25 22:51:20 -0300

    Using OnClick and Implementing the CLR Functionality

--- app/src/main/java/com/math/mycalculator/MainActivity.kt
+++ app/src/main/java/com/math/mycalculator/MainActivity.kt
@@ -3,15 +3,27 @@ package com.math.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
         Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+    }
+
+    fun onCLear(view: View) {
+        tvInput?.text = ""
     }
 }
